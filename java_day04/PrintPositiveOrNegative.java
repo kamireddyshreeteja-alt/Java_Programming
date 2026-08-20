6649935c75b118c78620115efb873df92aedc823
Shree Teja K <kamireddyshreeteja@gmail.com>
2026-08-20 20:28:54 +0530

    Added Java Day 4 programs

---
+++ java_day04/PrintPositiveOrNegative.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class PrintPositiveOrNegative{
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter a number: ");
+      int num = sc.nextInt();
+
+      if(num == 0){
+        System.out.println("Number is equal to zero");
+      }else if(num > 0){
+        System.out.println("Number is positive");
+      }else{
+        System.out.println("Number is negative");
+      }
+  }
+}
---
+++ java_day04/PrintPrimeNo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class PrintPrimeNo {
+  public static void main (String [] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int num = sc.nextInt();
+    int count = 0;
+
+    for(int i = 1; i <= num;i++){
+      if(num % i == 0){
+        count++;
+      }
+    }
+    if(count == 2){
+      System.out.println("it is a prime number");
+    }else{
+      System.out.println("It is not a prime number");
+    }
+  }
+}
